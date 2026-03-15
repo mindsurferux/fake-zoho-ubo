91d3b84783692d7bfc179427ce35fae93de1e81b
Mindsurfer <243607972+mindsurferux@users.noreply.github.com>
2026-03-14 22:00:01 -0300

    vercel deploy

--- migrate-webhooks.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-import { initSchema } from './src/db/connection.js';
-
-initSchema()
-  .then(() => console.log('Schema updated (webhooks table created)'))
-  .catch(e => console.error(e));
